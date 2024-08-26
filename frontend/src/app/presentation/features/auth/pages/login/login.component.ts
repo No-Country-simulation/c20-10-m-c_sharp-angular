66ffc51636e8962269e64e39540c7354225c9b1a
hiram_chavez <hiram.chavez@dataiq.com.mx>
2024-08-26 17:43:29 -0600

    add template

--- frontend/src/app/presentation/features/auth/pages/login/login.component.ts
+++ frontend/src/app/presentation/features/auth/pages/login/login.component.ts
@@ -1,11 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RouterLink } from '@angular/router';
+import { ButtonDirective } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, PasswordModule, CheckboxModule, RouterLink, ButtonDirective, Ripple, InputTextModule],
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [`
+        :host ::ng-deep .pi-eye,
+        :host ::ng-deep .pi-eye-slash {
+            transform:scale(1.6);
+            margin-right: 1rem;
+            color: var(--primary-color) !important;
+        }
+    `]
 })
-export default class LoginComponent {}
+export default class LoginComponent {
+  password!: string;
+}
