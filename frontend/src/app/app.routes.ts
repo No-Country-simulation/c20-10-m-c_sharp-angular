228841cc08214cc9781a4f530c527a75ac25a6d2
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-08-27 10:07:35 -0300

    update: The “presentation” folder has been removed to simplify the structure.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
   {
     path: 'home',
     title: 'Home',
-    loadComponent: () => import('./presentation/features/home/pages/home.component'),
+    loadComponent: () => import('./features/landing/pages/home/home.component'),
   },
   {
     path: 'auth',
@@ -22,18 +22,17 @@ export const routes: Routes = [
       {
         path: 'login',
         title: 'Iniciar sesión',
-        loadComponent: () => import('./presentation/features/auth/pages/login/login.component'),
+        loadComponent: () => import('./features/auth/pages/login/login.component'),
       },
       {
         path: 'register',
         title: 'Registrarse',
-        loadComponent: () =>
-          import('./presentation/features/auth/pages/register/register.component'),
+        loadComponent: () => import('./features/auth/pages/register/register.component'),
       },
       {
         path: 'forgot',
         title: 'Restablecer contraseña',
-        loadComponent: () => import('./presentation/features/auth/pages/forgot/forgot.component'),
+        loadComponent: () => import('./features/auth/pages/forgot/forgot.component'),
       },
     ],
   },
