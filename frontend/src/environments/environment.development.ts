e74f103ec5e70fb166b55b32e4e8738c6b864961
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-08-28 15:20:20 -0300

    feat: Environments

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  BASE_URL: 'http://www.contratapp.somee.com/',
+  ENDPOINT: {
+    LOGIN: 'identity/login',
+    REGISTER: 'identity/register',
+    USER: 'api/Usuario',
+  },
+  LOCALSTORAGE: {
+    JWT: 'jwt',
+  },
+};
