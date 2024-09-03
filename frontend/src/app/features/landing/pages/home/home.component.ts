e52aaf09a4992c4a1a8e413ed0feeea578912a61
Aixa Mengassini <verenice.aixa@gmail.com>
2024-09-03 13:43:54 -0300

    feat: create landing page

--- frontend/src/app/features/landing/pages/home/home.component.ts
+++ frontend/src/app/features/landing/pages/home/home.component.ts
@@ -8,36 +8,17 @@ import { AuthService } from './../../../../core/services/auth.service';
 import { SidebarModule } from 'primeng/sidebar';
 import { TopbarComponent } from '../../../../shared/components/topbar/topbar.component';
 
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, RouterModule, ButtonModule, SidebarModule, TopbarComponent],
-  template: `
-    @let authStatus = authService.authStatus();
-
-    @let status = authStatus ? 'Usuario autenticado' : 'Usuario no autenticado';
-
-    <div class="w-full">
-      <app-topbar />
-    </div>
-    <div class="flex flex-column justify-content-center align-items-center">
-      <h1>Test</h1>
-      <p class="text-3xl">
-        Status: <span [class]="authStatus ? 'text-green-500' : 'text-red-500'">{{ status }}</span>
-      </p>
-      @if (!authStatus) {
-        <p-button label="Iniciar sesión" outlined="true" routerLink="/auth/login" />
-      } @else {
-        <p-button
-          label="Cerrar sesión"
-          outlined="true"
-          severity="danger"
-          (click)="authService.logout()" />
-      }
-    </div>
-  `,
+  templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  
 })
 export default class HomeComponent {
+  
   public readonly authService = inject(AuthService);
 }
