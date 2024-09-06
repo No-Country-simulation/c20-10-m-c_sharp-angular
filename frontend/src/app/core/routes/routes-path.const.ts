87a6b108fa63c734db665b9a10f77fd9e15a55d9
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-06 13:08:29 -0300

    feat: Routes dictionary

---
+++ frontend/src/app/core/routes/routes-path.const.ts
@@ -0,0 +1,16 @@
+export const ROUTES_PATH = {
+  LANDING_HOME: 'inicio',
+  LANDING_BROWSER: 'explorar',
+  LANDING_BROWSER_CATEGORIES: 'explorar/categoria',
+  LANDING_BROWSER_CATEGORIES_ID: 'explorar/categoria/:categoryName',
+  LANDING_PROFILE_PROFESSIONAL: '',
+  LANDING_PROFILE_CLIENT: '',
+
+  DASHBOARD_MESSAGES: 'mensajes',
+  DASHBOARD_MESSAGES_INBOX: 'mensajes/:id',
+
+  AUTH_LOGIN: 'iniciar-sesion',
+  AUTH_REGISTER: 'registrarse',
+  AUTH_REGISTER_PROFESSIONAL: 'registro-profesional',
+  AUTH_FORGOT_PASSWORD: 'restablecer-contrase%C3%B1a',
+} as const;
