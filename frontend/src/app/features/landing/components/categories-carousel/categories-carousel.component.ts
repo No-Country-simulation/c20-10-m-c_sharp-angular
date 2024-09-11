aa7927a11c4be0ec1b14ebd7a6fd29d2ef34a3d6
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-11 13:49:46 -0300

    update: The component was updated implementing the swiper library

--- frontend/src/app/features/landing/components/categories-carousel/categories-carousel.component.ts
+++ frontend/src/app/features/landing/components/categories-carousel/categories-carousel.component.ts
@@ -1,74 +1,84 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CarouselModule } from 'primeng/carousel';
-import { ButtonModule  } from 'primeng/button';
-import { TagModule } from 'primeng/tag';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-categories-carousel',
   standalone: true,
-  imports: [CommonModule, CarouselModule, ButtonModule, TagModule ],
-  templateUrl:"./categories-carousel.component.html",
-  styles: `
+  imports: [CommonModule, RouterLink],
+  template: `
+    <swiper-container pagination="false" slides-per-view="1.5" space-between="24" free-mode="true">
+      @for (item of carouselImages; track $index) {
+        <swiper-slide class="relative custom-ml cursor-pointer" [routerLink]="item.route">
+          <div class="overflow-hidden border-round-md">
+            <img class="w-full" [src]="item.src" [alt]="item.alt" />
+            <div class="absolute top-0 left-0 w-full h-full custom-bg border-round-md"></div>
+            <h2 class="absolute custom-position text-white text-3xl">{{ item.category_label }}</h2>
+          </div>
+        </swiper-slide>
+      }
+    </swiper-container>
   `,
+  styles: `
+    .custom-ml {
+      &:first-child {
+        margin-left: 1.5rem;
+      }
+    }
+    .custom-bg {
+      background: linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.1), transparent);
+    }
+    .custom-position {
+      left: 1.25rem;
+      bottom: -0.5rem;
+    }
+  `,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoriesCarouselComponent {
-  items = [{}, {}, {}, {}];
-
-  products : any = [
+  /**
+   * @todo El category_label enrealidad deberia seguir la estructura de la
+   * interfaz categories o debe apuntar al de la categoria
+   */
+  public readonly carouselImages = [
     {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Hogar',
-      description: 'Hogar',
-      image: 'assets/images/landing-page/HOGAR.png',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5
+      id: 0,
+      category_label: 'Hogar',
+      route: '/explorar/categoria/hogar',
+      src: '/assets/images/landing-page/carousel-categories/0.png',
+      alt: '',
     },
     {
-      id: '1000',
-      code: 'f230fh0g3',
-      name: 'Belleza',
-      description: 'Belleza',
-      image: 'assets/images/landing-page/Belleza.png',
-      price: 65,
-      category: 'Accessories',
-      quantity: 24,
-      inventoryStatus: 'INSTOCK',
-      rating: 5
-    }
+      id: 1,
+      category_label: 'Automóvil',
+      route: '/explorar/categoria/automovil',
+      src: '/assets/images/landing-page/carousel-categories/1.png',
+      alt: '',
+    },
+    {
+      id: 2,
+      category_label: 'Cuidados',
+      route: '/explorar/categoria/cuidados',
+      src: '/assets/images/landing-page/carousel-categories/2.png',
+      alt: '',
+    },
+    {
+      id: 3,
+      category_label: 'Mascotas',
+      route: '/explorar/categoria/mascotas',
+      src: '/assets/images/landing-page/carousel-categories/3.png',
+      alt: '',
+    },
+    {
+      id: 4,
+      category_label: 'Belleza',
+      route: '/explorar/categoria/belleza',
+      src: '/assets/images/landing-page/carousel-categories/4.png',
+      alt: '',
+    },
   ];
-
-    responsiveOptions: any[] | undefined;
-
-    constructor() {}
-
-    ngOnInit() {
-        // this.productService.getProductsSmall().then((products) => {
-        //     this.products = products;
-        // });
-
-        this.responsiveOptions = [
-            {
-                breakpoint: '1199px',
-                numVisible: 1,
-                numScroll: 1
-            },
-            {
-                breakpoint: '991px',
-                numVisible: 2,
-                numScroll: 1
-            },
-            {
-                breakpoint: '767px',
-                numVisible: 1,
-                numScroll: 1
-            }
-        ];
-    }
-
 }
