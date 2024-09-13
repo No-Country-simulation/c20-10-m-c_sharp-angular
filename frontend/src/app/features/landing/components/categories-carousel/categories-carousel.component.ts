38d5f0fd2892ba90dd23d201f3611cd843eb81fb
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-13 09:49:07 -0300

    update: Now uses a reusable component and dynamic categories

--- frontend/src/app/features/landing/components/categories-carousel/categories-carousel.component.ts
+++ frontend/src/app/features/landing/components/categories-carousel/categories-carousel.component.ts
@@ -1,23 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { register } from 'swiper/element/bundle';
 register();
 
+import { CardImgComponent } from '../../components';
+import { CategoryResponse } from '../../../../core/interfaces';
+
 @Component({
   selector: 'app-categories-carousel',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, CardImgComponent],
   template: `
     <swiper-container pagination="false" slides-per-view="1.5" space-between="24" free-mode="true">
-      @for (item of carouselImages; track $index) {
-        <swiper-slide class="relative custom-ml cursor-pointer" [routerLink]="item.route">
-          <div class="overflow-hidden border-round-md">
-            <img class="w-full" [src]="item.src" [alt]="item.alt" />
-            <div class="absolute top-0 left-0 w-full h-full custom-bg border-round-md"></div>
-            <h2 class="absolute custom-position text-white text-3xl">{{ item.category_label }}</h2>
-          </div>
+      @for (item of dataWithRoutes(); track $index) {
+        <swiper-slide class="relative custom-ml cursor-pointer">
+          <app-card-img [data]="item" />
         </swiper-slide>
       }
     </swiper-container>
@@ -28,57 +33,28 @@ register();
         margin-left: 1.5rem;
       }
     }
-    .custom-bg {
-      background: linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.1), transparent);
-    }
-    .custom-position {
-      left: 1.25rem;
-      bottom: -0.5rem;
+    @media (width > 767px) {
+      .custom-ml {
+        &:first-child {
+          margin-left: 0;
+        }
+      }
     }
   `,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoriesCarouselComponent {
-  /**
-   * @todo El category_label enrealidad deberia seguir la estructura de la
-   * interfaz categories o debe apuntar al de la categoria
-   */
-  public readonly carouselImages = [
-    {
-      id: 0,
-      category_label: 'Hogar',
-      route: '/explorar/categoria/hogar',
-      src: '/assets/images/landing-page/carousel-categories/0.png',
-      alt: '',
-    },
-    {
-      id: 1,
-      category_label: 'Automóvil',
-      route: '/explorar/categoria/automovil',
-      src: '/assets/images/landing-page/carousel-categories/1.png',
-      alt: '',
-    },
-    {
-      id: 2,
-      category_label: 'Cuidados',
-      route: '/explorar/categoria/cuidados',
-      src: '/assets/images/landing-page/carousel-categories/2.png',
-      alt: '',
-    },
-    {
-      id: 3,
-      category_label: 'Mascotas',
-      route: '/explorar/categoria/mascotas',
-      src: '/assets/images/landing-page/carousel-categories/3.png',
-      alt: '',
-    },
-    {
-      id: 4,
-      category_label: 'Belleza',
-      route: '/explorar/categoria/belleza',
-      src: '/assets/images/landing-page/carousel-categories/4.png',
-      alt: '',
-    },
-  ];
+export class CategoriesCarouselComponent implements OnInit {
+  public readonly data = input.required<CategoryResponse[]>();
+  public readonly dataWithRoutes = signal<CategoryResponse[]>([]);
+
+  ngOnInit(): void {
+    const dataWithRoutes = this.data().map(data => {
+      return {
+        ...data,
+        route: 'explorar/categoria/',
+      };
+    });
+    this.dataWithRoutes.set(dataWithRoutes);
+  }
 }
