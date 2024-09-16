6538c8b29916ac54e18efdb64a828cffeeb5882c
Mario Roberto Molano <mariormolano@yahoo.com>
2024-09-16 09:39:42 -0500

    Ajuste interfaces

--- frontend/src/app/core/interfaces/user.interface.ts
+++ frontend/src/app/core/interfaces/user.interface.ts
@@ -1,5 +1,5 @@
-import { Speciality } from './speciality.interface';
 import { MessageResponse } from './message.interface';
+import { UserSpeciality } from './user-specialities.intefaces';
 
 export interface User {
   id: string;
@@ -18,29 +18,15 @@ export interface User {
   creditCard: boolean;
   cash: boolean;
   userSpecialities: UserSpeciality[];
-  srcImage:         string;
+  srcImage: string;
 }
 
 export type UserUpdate = Omit<User, 'id' | 'email'>;
-
-export interface UserSpeciality {
-  id:           number;
-  isActive:     boolean;
-  createdAt:    Date;
-  idUser:       string;
-  user:         string;
-  idSpeciality: number;
-  speciality:   Speciality;
-  title:        string;
-  text:         string;
-  area:         string;
-}
-
 export interface UserMessages {
-  id:        number;
+  id: number;
   idOtherUser: string;
-  name:      string;
-  image:     string;
+  name: string;
+  image: string;
   createdAt: Date;
-  messages:  MessageResponse[];
+  messages: MessageResponse[];
 }
