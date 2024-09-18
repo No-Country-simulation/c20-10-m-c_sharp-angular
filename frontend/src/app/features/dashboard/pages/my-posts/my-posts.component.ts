775ec720d3fb04a19f41da5e1255ae0c4dc71c0c
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-18 11:41:29 -0300

    feat: My posts component

--- frontend/src/app/features/dashboard/pages/my-posts/my-posts.component.ts
+++ frontend/src/app/features/dashboard/pages/my-posts/my-posts.component.ts
@@ -1,36 +1,89 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
 import { RatingModule } from 'primeng/rating';
 import { TagModule } from 'primeng/tag';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { UserService, UserSpecialitiesService } from '@app/core/services';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { UserSpeciality } from '@app/core/interfaces';
 
 @Component({
   selector: 'app-my-posts',
   standalone: true,
-  imports: [CommonModule, DataViewModule, TagModule, RatingModule, ButtonModule, CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    DataViewModule,
+    TagModule,
+    RatingModule,
+    ButtonModule,
+    CommonModule,
+    ConfirmDialogModule,
+  ],
   templateUrl: './my-posts.component.html',
   styleUrl: './my-posts.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class MyPostsComponent {
-  layout: 'list' | 'grid' = 'list';
+export default class MyPostsComponent implements OnInit {
+  public readonly userSpecialitiesService = inject(UserSpecialitiesService);
+  public readonly confirmationService = inject(ConfirmationService);
+  public readonly messageService = inject(MessageService);
+  public readonly userService = inject(UserService);
+  public readonly destroyRef = inject(DestroyRef);
 
-  products = [{}, {}, {}, {}, {}, {}, {}, {}];
+  public readonly postsData = signal<UserSpeciality[]>([]);
 
-  getSeverity(product: any) {
-    switch (product.inventoryStatus) {
-      case 'INSTOCK':
-        return 'success';
+  ngOnInit(): void {
+    this.getPosts();
+  }
 
-      case 'LOWSTOCK':
-        return 'warning';
+  public deletePost(id: number) {
+    this.confirmationService.confirm({
+      message: 'Esta accion es irreversible <br> ¿Desea continuar?',
+      header: 'Estas a punto de borrar esta publicacion',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+      acceptButtonStyleClass: 'p-button-danger p-button-outlined ',
+      rejectButtonStyleClass: 'p-button-primary',
+      accept: () => {
+        // this.userSpecialitiesService.deleteUserSpecialitiesById(id)
+        this.messageService.add({
+          key: 'toast',
+          severity: 'success',
+          summary: 'Se ha eliminado',
+          detail: 'El post se ha eliminado correctamente',
+        });
+        this.getPosts();
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rejected',
+          detail: 'You have rejected',
+          life: 3000,
+        });
+      },
+    });
+  }
 
-      case 'OUTOFSTOCK':
-        return 'danger';
-
-      default:
-        return null;
-    }
+  public getPosts(): void {
+    this.userService
+      .getUserData()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(res => {
+        this.postsData.set(res.userSpecialities);
+      });
   }
 }
