356b08cafcdc4be26f6ebe300abea644bf275c1c
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-18 20:42:01 -0300

    feat: Now you can do a search by address and get the data of the place.

--- frontend/src/app/core/services/user-location.service.ts
+++ frontend/src/app/core/services/user-location.service.ts
@@ -1,16 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { shareReplay } from 'rxjs';
+import { of, shareReplay, tap } from 'rxjs';
+import { SessionStorageService } from './session-storage.service';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserLocationService {
+  private readonly sessionStorageService = inject(SessionStorageService);
   private readonly http = inject(HttpClient);
-  private readonly ipApiEndpoint =
-    'http://ip-api.com/json?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon';
+
+  private readonly ipApiEndpoint = environment.LOCATION.CURRENT_USER_LOCATION;
+  private readonly addressAutocompleteEndpoint = environment.LOCATION.ADDRESS_AUTOCOMPLETE_BASE_URL;
+  private readonly addressAutocompleteKey = environment.LOCATION.ADDRESS_AUTOCOMPLETE_KEY;
 
   public getUserLocation() {
     return this.http.get(this.ipApiEndpoint).pipe(shareReplay(1));
+  }
+
+  public addressAutocomplete(query: string) {
+    const cachedResponse = this.sessionStorageService.get(`${encodeURIComponent(query)}key`);
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+    const headers = new HttpHeaders({
+      'x-placekit-api-key': this.addressAutocompleteKey,
+      'Content-Type': 'application/json',
+    });
+    const body = { query };
+    return this.http.post(this.addressAutocompleteEndpoint, body, { headers }).pipe(
+      tap(res => {
+        this.sessionStorageService.set(`${encodeURIComponent(query)}key`, res);
+      })
+    );
   }
 }
