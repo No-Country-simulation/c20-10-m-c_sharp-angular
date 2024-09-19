deda2e442abb1edab1c5a7a581814c85fde96357
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-19 13:19:58 -0300

    update: The map is added if there are coordinates.

--- frontend/src/app/features/landing/components/google-map/google-map.component.ts
+++ frontend/src/app/features/landing/components/google-map/google-map.component.ts
@@ -2,15 +2,13 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, input, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
-import { Location } from '../../../../core/interfaces';
-
 @Component({
   selector: 'app-google-map',
   standalone: true,
   imports: [CommonModule],
   template: `
     <iframe
-      class="border-round border-1 border-blue-400 h-10rem"
+      class="border-round border-1 border-blue-400 h-10rem w-full"
       frameborder="0"
       scrolling="no"
       marginheight="0"
@@ -22,12 +20,13 @@ import { Location } from '../../../../core/interfaces';
 })
 export class GoogleMapComponent implements OnInit {
   private readonly domSanitizer = inject(DomSanitizer);
-  public readonly locationData = input.required<Location>();
+  public readonly lat = input.required<string>();
+  public readonly lon = input.required<string>();
   public safeUrl!: SafeResourceUrl;
 
   ngOnInit(): void {
     this.safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(
-      `https://maps.google.com/maps?q=${this.locationData().lat},${this.locationData().lon}&hl=es&z=15&output=embed`
+      `https://maps.google.com/maps?q=${this.lat()},${this.lon()}&hl=es&z=15&output=embed`
     );
   }
 }
