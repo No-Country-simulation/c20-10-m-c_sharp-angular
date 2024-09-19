685a16e2e71f4eb24d3090d34da818076fc4d008
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-09-19 13:20:59 -0300

    update index

--- frontend/src/app/shared/components/index.ts
+++ frontend/src/app/shared/components/index.ts
@@ -1,3 +1,7 @@
+export * from './btn-navigate-prev/btn-navigate-prev.component';
 export * from './dialog/generic-dialog/generic-dialog.component';
 export * from './logo/logo.component';
+export * from './profile-avatar/profile-avatar.component';
+export * from './search-box/search-box.component';
+export * from './spinner/spinner.component';
 export * from './topbar/topbar.component';
